ef30c45e7da3fe718998620fe88ef68f7014751e
Eduardo Esteban Alvarez Castañeda <soy@eduardoalvarez.dev>
2024-07-17 22:34:57 -0400

    chore: some minor changes

--- src/app/_sections/Footer.tsx
+++ src/app/_sections/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
 		<footer>
 			<div className="flex justify-end bg-slate-900 text-white items-center min-h-14 p-5">
 				<p className="text-sm font-normal text-pretty text-end">
-					2024 Edig frontend challenge. Developed by
+					2024 Edig FullStack Challenge. Developed by
 					<a href="https://eduardoalvarez.dev" target="_blank" rel="noopener noreferrer" className="font-bold ml-1">
 						Eduardo Álvarez
 					</a>
--- src/app/_sections/Header.tsx
+++ src/app/_sections/Header.tsx
@@ -4,7 +4,6 @@ export default function Header() {
 	return (
 		<header className="w-full bg-slate-900 text-white p-4 shadow flex justify-center items-center gap-5">
 			<Image src="/edig-logo.png" alt="Logo" width={100} height={47} />
-			<h1>TODO App Challenge</h1>
 		</header>
 	);
 }
