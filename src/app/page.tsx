f27b4a42d0c6a04bdd23372810c960f62081396b
Eduardo Esteban Alvarez Castañeda <soy@eduardoalvarez.dev>
2024-07-17 20:35:08 -0400

    style: improve main layout styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,13 @@ import TaskList from './_sections/TaskList';
 
 export default function Home() {
 	return (
-		<main className="w-full h-full grid grid-rows-[auto,1fr,auto] ">
+		<main className="w-full h-full grid grid-rows-[auto,1fr,auto] overflow-x-hidden">
 			<Header />
-			<section className="w-full max-w-[640px] h-full flex flex-col items-center gap-8 mx-auto pt-10">
-				<CreateNewTask />
-				<TaskList />
+			<section className="w-full h-full  overflow-y-auto">
+				<div className="w-full max-w-[640px] gap-8 flex flex-col items-center mx-auto pt-10">
+					<CreateNewTask />
+					<TaskList />
+				</div>
 			</section>
 			<Footer />
 		</main>
